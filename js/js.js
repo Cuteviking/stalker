a48cdee60744f4af2b11fdac5152f4a6d3224faf
Norrbrinken <pierrenorrbrink@gmail.com>
2015-09-24 15:16:40 +0200

    friends

--- js/js.js
+++ js/js.js
@@ -9,8 +9,9 @@ save friends location
  
 */
 function init(){
-	    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(saveLogLat);
+	
+	if (navigator.geolocation) { // GPS ON 
+        navigator.geolocation.getCurrentPosition(saveLogLat); // fetch coords
     } else {
         // todo: inform that geolocation is not on. 
 		console.log("Geolocation is not supported");
@@ -18,47 +19,66 @@ function init(){
 }
 
 function saveLogLat(myLatLng){
-	//todo: save to database
-	//save
-	localStorage.latitude = myLatLng.coords.latitude;
-	localStorage.longitude = myLatLng.coords.longitude;
 	
-	initMap(myLatLng);
+	var myDataRef = new Firebase('https://sweltering-torch-5016.firebaseio.com/'); //firebase
+	
+	//todo: check if internet .username needs to be defined
+	//save network . 
+	myDataRef.on("child_added", function(snapshot) {
+			if(snapshot.val().user.name == localStorage.username){
+				console.log(snapshot.val().user.name);
+				var myDataRefUser = new Firebase('https://sweltering-torch-5016.firebaseio.com/' + snapshot.key() + '/user/data');
+				console.log('https://sweltering-torch-5016.firebaseio.com/' + snapshot.key() + '/user/data');
+				myDataRefUser.update({lat: myLatLng.coords.latitude, lng: myLatLng.coords.longitude});
+				
+				//save local
+				localStorage.latitude = myLatLng.coords.latitude;
+				localStorage.longitude = myLatLng.coords.longitude;
+				
+				initMap();
+			}
+	}, function (errorObject) {
+		console.log("The read failed: " + errorObject.code);
+	})	
 }
 
-function initMap(myLatLng){
+
+function initMap(){
 	//myLatLng = {lat: myLatLng.coords.latitude, lng: myLatLng.coords.longitude};
-	
-	
 	
 	// Create a map object
 	var map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: myLatLng.coords.latitude, lng: myLatLng.coords.longitude},
+		center: {lat: Number(localStorage.latitude), lng: Number(localStorage.longitude)},
 		scrollwheel: false,
 		zoom: 14
 	});
-	setUser(myLatLng, map);
+	setUser(map);
 }
 
-function setUser(myLatLng, map){
-	setMarker(myLatLng, map);
+function setUser(map){
+	setMarker(map, localStorage.latitude, localStorage.longitude, localStorage.username);
 	setFriends(map);
 }
 
 function setFriends(map){
-	/*for(var i=0; i<localStorage.friends.length; i++){
-		setMarker(localStorage.friends[i],map);
-	}*/
+	var myDataRef = new Firebase('https://sweltering-torch-5016.firebaseio.com/');
+	
+	myDataRef.on("child_added", function(snapshot) {
+		setMarker(map,snapshot.val().user.data.lat,snapshot.val().user.data.lng,snapshot.val().user.name);
+	}, function (errorObject) {
+		console.log("The read failed: " + errorObject.code);
+	})
+	
 }
 
 //---
 
-function setMarker(myLatLng, map){
+function setMarker(map, latitude, longitude, name){
 	// Create a marker and set its position
 	var marker = new google.maps.Marker({
 		map: map,
-		position: {lat: myLatLng.coords.latitude, lng: myLatLng.coords.longitude},
-		title: 'Hello World!'
+		position: {lat: Number(latitude), lng: Number(longitude)},
+		title: name
 	});
 	return 0;
 }
